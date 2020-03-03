e96632216735654a5a879298349c67db2d21eee9
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-03 14:41:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,7 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
-      elsif @@cart
+      else @@cart
     else
       resp.write "Path Not Found"
     end
